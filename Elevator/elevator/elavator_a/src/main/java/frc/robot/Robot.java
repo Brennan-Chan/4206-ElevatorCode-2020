8e55bb1b2af8a4468bf3ff9c11f6fc8298272ed9
mvu <kaisenbot@protonmail.com>
2019-10-14 13:23:12 -0500

    New elevator code (work in progress)
    Basic foundation of elevator code

---
+++ Elevator/elevator/elavator_a/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,83 @@
+
+package frc.robot;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+
+/*
+  The VM is configured to automatically run this class, and to call the
+  functions corresponding to each mode, as described in the TimedRobot
+  documentation. If you change the name of this class or the package after
+  creating this project, you must also update the build.gradle file in the
+  project.
+*/
+public class Robot extends TimedRobot {
+  //Subsystem instancing
+  public static Elevator up = new Elevator();
+
+  public static RobotMap rm = new RobotMap();
+  public static OI oi;
+
+  Command m_autonomousCommand;
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+  //INIT METHODS
+  @Override
+  public void robotInit(){
+    oi = new OI();
+    //TODO: Add drive modes (autonomous) to SmartDashboard here and in autonomousInit()
+    SmartDashboard.putData("Teleop", m_chooser);
+
+    
+  }  
+
+  @Override
+  public void disabledInit(){
+    System.out.println("You DIED");
+  }
+
+  @Override
+  public void teleopInit(){
+    //Cancels autonomous mode upon initializing operator control
+    if (m_autonomousCommand != null){
+      m_autonomousCommand.cancel();
+    }
+  }
+
+  @Override
+  public void autonomousInit(){
+    m_autonomousCommand = m_chooser.getSelected();
+    if (m_autonomousCommand != null){
+      m_autonomousCommand.start();
+    }
+  }
+
+  //PERIODIC FUNCTIONS
+  @Override
+  public void robotPeriodic(){
+    
+  }
+
+  @Override
+  public void disabledPeriodic(){
+    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void teleopPeriodic(){
+    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void testPeriodic(){
+  }
+}
